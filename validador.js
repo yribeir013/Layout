45e8fd47a1f7a1611c9bd58ee4268d040c193235
Yngrid Ribeiro Almeida <182200148@senacinformatica.br>
2023-02-09 13:41:30 -0300

    alteraçoes

--- validador.js
+++ validador.js
@@ -25,9 +25,9 @@ function mascaraCPF(i){
      i.value = cpf.substring(0,cpf.length-1);
      return;
     }
-    i.setAttribute("maxlength", "15");
+    i.setAttribute("maxlength", "14");
   
-    if (cpf.length == 3){  /*Inclui máscara do telefone*/
+    if (cpf.length == 3){  
         i.value = i.value + ".";
     }
     if (cpf.length == 7){ 
@@ -45,7 +45,7 @@ function mascaraCNPJ(i){
     }
     i.setAttribute("maxlength", "18");
   
-    if (cnpj.length == 2){  /*Inclui máscara do telefone*/
+    if (cnpj.length == 2){  
         i.value = i.value + ".";
     }
     if (cnpj.length == 6){ 
@@ -58,9 +58,16 @@ function mascaraCNPJ(i){
         i.value = i.value + "-";
 }
 }
+
 function validar (){
     var nome = formUser.nome.value;
-    
+    var email = formUser.email.value;
+    var telefone = formUser.telefone.value;
+    var endereco = formUser.endereco.value;
+    var cpf = formUser.cpf.value;
+    var cnpj = formUser.cnpj.value;
+    var senha = formUser.senha.value;
+    var confirmaSenha = formUser.confirmaSenha.value;
     
     if (nome == "" || nome.length <= 3) {
         alert("Informe o nome completo");
@@ -68,6 +75,62 @@ function validar (){
         return false;
         
     } 
-   
+
+    if (email == "" || email.length <= 10) {
+        alert("Informe o e-mail completo");
+        formUser.email.focus();
+        return false;
     
+    } 
+
+    if (telefone == "" || telefone.length <= 15) {
+        alert("Informe o celular completo");
+        formUser.telefone.focus();
+        return false;
+        
+    } 
+
+    if (endereco == "" || endereco.length <= 10) {
+        alert("Informe o endereço completo");
+        formUser.endereco.focus();
+        return false;
+        
+    } 
+
+    if(cpf == "" && cnpj == "" ){
+        alert("Selecione um documento");
+        return false;
+    }
+
+    if(cnpj != "" && cpf == ""){
+        if(cnpj.length != 18){
+            alert("CNPJ incorreto!");
+            formUser.cnpj.focus();
+            return false;
+        }
+    }
+
+    if(cpf != "" && cnpj != ""){
+        if(cpf.length != 14){
+            alert("CPF incorreto!");
+            formUser.cpf.focus();
+            return false;
+        }
+    }
+
+    if(senha == "" || senha.length <8){
+        alert("Senha não atende aos requisitos!");
+        formUser.senha.focus();
+        return false;
+}
+
+    if (senha != "" && confirmaSenha != "") {
+        if(senha != confirmaSenha){
+        alert("Senhas diferentes!");
+        formUser.senha.focus();
+        return false;
+        
+    }
+
+    }
 }
